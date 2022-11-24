2b7225682a2b8c2c364333cd676d23c8bd9fcd3b
vinicius <viniciusmartinsdacunha1@gmail.com>
2022-11-24 20:49:22 -0300

    feat:Component Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,12 @@
 import styles from "./styles.css"
 
-export function Button({title}){
-  return(
+//Title = Nome interno do botão
+//Size = Se o tamanho da fonte for diferente (Header e Formulário)
+
+export function Button({title, size = false}){
+  return( 
       <div className={styles.buttons}>
-        <button>{title}</button>
+        <button className={`${size}`}>{title}</button>
       </div>
   )
 }
