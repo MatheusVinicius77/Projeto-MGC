a391b7fd25b11e1071fcdba20fc47f09d74f0462
Danielle da Luz <daniln2209@gmail.com>
2022-11-26 15:44:47 +0000

    feat: adiciona prop de background no header

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -5,26 +5,47 @@ import styles from "./styles.module.css";
 
 import { IconeJanela } from "./IconeJanela";
 
-export function Card ({listaCoresCirculos, imagem, textoAlternativoImagem, sinal, textoAlternativoSinal, corIconeJanela}) {
-    return (
-        <article className={styles.card}>
-            <header className={styles.cardHeader}>
-                <div className={`${styles.circulosWrapper}`}>
-                    {
-                        listaCoresCirculos.map(cor => {
-                            return <span className={`${styles.circulo} ${cor}`}></span>;
-                        })
-                    }
-                </div>
-                <div className={styles.icones}>
-                    <img className={styles.icone} src={iconeMinimizacao} alt="ícone de minimização" />
-                    <IconeJanela cor={corIconeJanela} />
-                    <img className={styles.icone} src={iconeFechar} alt="ícone de fechamento" />
-                </div>
-            </header>
-            <img className={styles.cardImg} src={imagem} alt={textoAlternativoImagem} />
-            <img className={styles.cardSinal} src={sinal} alt={textoAlternativoSinal}  />
-        </article>
-    );
+export function Card({
+  corFundoHeader,
+  listaCoresCirculos,
+  imagem,
+  textoAlternativoImagem,
+  sinal,
+  textoAlternativoSinal,
+  corIconeJanela,
+}) {
+  return (
+    <article className={styles.card}>
+      <header className={`${styles.cardHeader} ${corFundoHeader}`}>
+        <div className={`${styles.circulosWrapper}`}>
+          {listaCoresCirculos.map((cor) => {
+            return <span className={`${styles.circulo} ${cor}`}></span>;
+          })}
+        </div>
+        <div className={styles.icones}>
+          <img
+            className={styles.icone}
+            src={iconeMinimizacao}
+            alt="ícone de minimização"
+          />
+          <IconeJanela cor={corIconeJanela} />
+          <img
+            className={styles.icone}
+            src={iconeFechar}
+            alt="ícone de fechamento"
+          />
+        </div>
+      </header>
+      <img
+        className={styles.cardImg}
+        src={imagem}
+        alt={textoAlternativoImagem}
+      />
+      <img
+        className={styles.cardSinal}
+        src={sinal}
+        alt={textoAlternativoSinal}
+      />
+    </article>
+  );
 }
-
