d072f6e32275d74809ba09cefa759471c8ddb0a4
Danielle da Luz <daniln2209@gmail.com>
2022-11-26 04:13:38 +0000

    feat: adiciona ícones como componentes

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,15 +1,5 @@
 import iconeMinimizacao from "../../assets/imgs/icones/traco.svg";
 import iconeFechar from "../../assets/imgs/icones/x.svg";
-import iconeJanelaAmarela from "../../assets/imgs/icones/janela-amarelo.svg";
-import iconeJanelaRosa from "../../assets/imgs/icones/janela-rosa.svg";
-import iconeJanelaVerde from "../../assets/imgs/icones/janela-verde.svg";
-import cursor from "../../assets/imgs/icones/seta-branca-verde.svg";
-import exclamacaoAmareloVerde from "../../assets/imgs/sinais/exclamacao-amarelo-verde.svg";
-import exclamacaoAzulVerde from "../../assets/imgs/sinais/exclamacao-azul-verde.svg";
-import exclamacaoRosaVerde from "../../assets/imgs/sinais/exclamacao-rosa-verde.svg";
-import exclamacaoRoxoRosa from "../../assets/imgs/sinais/exclamacao-roxo-rosa.svg";
-import exclamacaoRoxoVerde from "../../assets/imgs/sinais/exclamacao-roxo-verde.svg";
-import interrogacaoAmareloRosa from "../../assets/imgs/sinais/interrogacao-rosa-amarelo.svg";
 
 import styles from "./styles.module.css";
 
