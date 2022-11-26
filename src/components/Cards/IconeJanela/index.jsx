8754d49188de75846eeab8b77738d15e8ac38222
Danielle da Luz <daniln2209@gmail.com>
2022-11-26 04:17:14 +0000

    feat: cria componente de ícone de janela

---
+++ src/components/Cards/IconeJanela/index.jsx
@@ -0,0 +1,8 @@
+export function IconeJanela({ cor }) {
+    return (
+        <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="7.93933" y="8.78784" width="17.2121" height="17.2121" fill={cor} stroke="black" stroke-width="4" />
+            <rect x="2" y="2" width="17.2121" height="18.0606" fill={cor} stroke="black" stroke-width="4" />
+        </svg>
+    );
+}
