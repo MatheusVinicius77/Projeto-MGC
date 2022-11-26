649e7220f65776f91f1d182ad021eaec3d54bed0
Danielle da Luz <daniln2209@gmail.com>
2022-11-26 01:57:54 +0000

    fix: corrige url de ícone de janela

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,7 +1,17 @@
-import iconeMinimizacao from "../../assets/imgs/icones/janela-amarelo.svg";
-import iconeFechar from "../../assets/imgs/icones/janela-amarelo.svg";
+import iconeMinimizacao from "../../assets/imgs/icones/traco.svg";
+import iconeFechar from "../../assets/imgs/icones/x.svg";
+import iconeJanelaAmarela from "../../assets/imgs/icones/janela-amarelo.svg";
+import iconeJanelaRosa from "../../assets/imgs/icones/janela-rosa.svg";
+import iconeJanelaVerde from "../../assets/imgs/icones/janela-verde.svg";
+import cursor from "../../assets/imgs/icones/seta-branca-verde.svg";
+import exclamacaoAmareloVerde from "../../assets/imgs/sinais/exclamacao-amarelo-verde.svg";
+import exclamacaoAzulVerde from "../../assets/imgs/sinais/exclamacao-azul-verde.svg";
+import exclamacaoRosaVerde from "../../assets/imgs/sinais/exclamacao-rosa-verde.svg";
+import exclamacaoRoxoRosa from "../../assets/imgs/sinais/exclamacao-roxo-rosa.svg";
+import exclamacaoRoxoVerde from "../../assets/imgs/sinais/exclamacao-roxo-verde.svg";
+import interrogacaoAmareloRosa from "../../assets/imgs/sinais/interrogacao-rosa-amarelo.svg";
 
-export function Cards ({listaCoresCirculos, imagem, textoAlternativoImagem, sinal, textoAlternativoSinal, corIconeJanela}) {
+export function Card ({listaCoresCirculos, imagem, textoAlternativoImagem, sinal, textoAlternativoSinal, iconeJanela}) {
     return (
         <article>
             <header>
@@ -14,8 +24,8 @@ export function Cards ({listaCoresCirculos, imagem, textoAlternativoImagem, sina
                 </div>
                 <div>
                     <img src={iconeMinimizacao} alt="ícone de minimização" />
-                    <img src={`../src/assets/imgs/icones/janela-${corIconeJanela}`} alt={`ícone de janela ${corIconeJanela}`} />
-                    <img src={iconeFechar} alt="ícone de minimização" />
+                    <img src={`../src/assets/imgs/icones/janela-${iconeJanela}`} alt="ícone de janela colorido" />
+                    <img src={iconeFechar} alt="ícone de fechamento" />
                 </div>
             </header>
             <img src={imagem} alt={textoAlternativoImagem} />
@@ -23,3 +33,4 @@ export function Cards ({listaCoresCirculos, imagem, textoAlternativoImagem, sina
         </article>
     );
 }
+
