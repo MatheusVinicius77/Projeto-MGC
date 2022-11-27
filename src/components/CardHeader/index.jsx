281b2e30eadb60157993e90517c07a475894277e
Danielle da Luz <daniln2209@gmail.com>
2022-11-27 00:16:30 +0000

    feat: adiciona classes no arquvo styles

--- src/components/CardHeader/index.jsx
+++ src/components/CardHeader/index.jsx
@@ -1,28 +1,34 @@
 import iconeMinimizacao from "../../assets/imgs/icones/traco.svg";
 import iconeFechar from "../../assets/imgs/icones/x.svg";
+
 import { IconeJanela } from "./IconeJanela";
 
-export function CardHeader ({corFundoHeader, listaCoresCirculos, corIconeJanela}) {
-    return (
-        <header className={`${styles.cardHeader} ${corFundoHeader} ${styles.sombra}`}>
-          <div className={`${styles.circulosWrapper}`}>
-            {listaCoresCirculos.map((cor) => {
-              return <span className={`${styles.circulo} ${cor}`}></span>;
-            })}
-          </div>
-          <div className={styles.icones}>
-            <img
-              className={styles.icone}
-              src={iconeMinimizacao}
-              alt="ícone de minimização"
-            />
-            <IconeJanela className={styles.icone} cor={corIconeJanela} />
-            <img
-              className={styles.icone}
-              src={iconeFechar}
-              alt="ícone de fechamento"
-            />
-          </div>
-        </header>
-    );
+export function CardHeader({
+  corFundoHeader,
+  listaCoresCirculos,
+  corIconeJanela,
+  className,
+}) {
+  return (
+    <header className={`${styles.cardHeader} ${corFundoHeader} ${className}`}>
+      <div className={styles.circulosWrapper}>
+        {listaCoresCirculos.map((cor) => {
+          return <span className={`${styles.circulo} ${cor}`}></span>;
+        })}
+      </div>
+      <div className={styles.icones}>
+        <img
+          className={styles.icone}
+          src={iconeMinimizacao}
+          alt="ícone de minimização"
+        />
+        <IconeJanela className={styles.icone} cor={corIconeJanela} />
+        <img
+          className={styles.icone}
+          src={iconeFechar}
+          alt="ícone de fechamento"
+        />
+      </div>
+    </header>
+  );
 }
