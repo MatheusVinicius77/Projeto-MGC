87ad8c9ae8af66fccf394defbab0f3f86c0d71c7
Danielle da Luz <daniln2209@gmail.com>
2022-11-27 00:36:44 +0000

    feat: adiciona componente card header no card

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,12 +1,9 @@
-import iconeMinimizacao from "../../assets/imgs/icones/traco.svg";
-import iconeFechar from "../../assets/imgs/icones/x.svg";
-
 import styles from "./styles.module.css";
-
-import { IconeJanela } from "./IconeJanela";
 import { IconeExclamacao } from "./IconeExclamacao";
 import { IconeInterrogacao } from "./IconeInterrogacao";
 import { IconeCursor } from "./IconeCursor";
+
+import { CardHeader } from "../CardHeader";
 
 export function Card({
   corFundoHeader,
@@ -20,26 +17,7 @@ export function Card({
   return (
     <div className={styles.cardWrapper}>
       <article className={styles.card}>
-        <header className={`${styles.cardHeader} ${corFundoHeader} ${styles.sombra}`}>
-          <div className={`${styles.circulosWrapper}`}>
-            {listaCoresCirculos.map((cor) => {
-              return <span className={`${styles.circulo} ${cor}`}></span>;
-            })}
-          </div>
-          <div className={styles.icones}>
-            <img
-              className={styles.icone}
-              src={iconeMinimizacao}
-              alt="ícone de minimização"
-            />
-            <IconeJanela className={styles.icone} cor={corIconeJanela} />
-            <img
-              className={styles.icone}
-              src={iconeFechar}
-              alt="ícone de fechamento"
-            />
-          </div>
-        </header>
+        <CardHeader corFundoHeader={corFundoHeader} listaCoresCirculos={listaCoresCirculos} corIconeJanela={corIconeJanela} />
         <img
           className={`${styles.cardImg}  ${styles.sombra}`}
           src={imagem}
