74987df026b21694501ec0d4dd13e99bf3333058
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2022-12-07 00:29:56 -0300

    feat:adiciona nome do autor inacabada

--- src/components/Depoimento/index.jsx
+++ src/components/Depoimento/index.jsx
@@ -19,7 +19,12 @@ export function Depoimento({
 
                 <div className={`${styles.cardHeader}  ${styles.sombra}`}>
                     <div className={styles.boxFoto}>
-                        
+                        {/* Por hora tem apenas o box, mas irei atualizar para ter imagens */}
+                    </div>
+
+                    <div className={styles.infoAutor}>
+                        <h2 className={styles.nomeAutor}>Nome de quem depõe</h2>
+                         {/* Por hora tem apenas o texto padrão no site, mas irei atualizar para ter o respectivo nome do autor */}
                     </div>
                 </div> 
             </article>
