6655f3568a38e2b6eb058fab79efa168c58b7fad
Danielle da Luz <daniln2209@gmail.com>
2022-12-24 22:44:18 +0000

    feat: cria página quem somos

---
+++ src/pages/QuemSomos/index.jsx
@@ -0,0 +1,12 @@
+import { Header } from "../../components/Header";
+
+export function QuemSomos() {
+  return (
+    <>
+      <Header />
+      <section>
+        <h1 className="title-1">Quem somos</h1>
+      </section>
+    </>
+  );
+}
