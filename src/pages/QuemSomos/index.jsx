cfc8518a558590f6c5bae038732142e6e47d1f25
Danielle da Luz <daniln2209@gmail.com>
2022-12-24 23:22:37 +0000

    feat: adiciona componentes text icon

--- src/pages/QuemSomos/index.jsx
+++ src/pages/QuemSomos/index.jsx
@@ -1,14 +1,21 @@
 import { Header } from "../../components/Header";
 import styles from "./styles.module.css";
+import { TextIcon } from "./Text-icon";
 
 export function QuemSomos() {
+  const missaoTexto = "Contribuir na construção do futuro das crianças, adolescentes e seus familiares promover transformações humanas, sociais e econômicas; viabilizar a justiça social através da educação e da profissionalização dos jovens que vivem em locais periféricos do Rio de Janeiro.";
+  const visaoTexto = "A educação, a arte e a cultura são os meios de emergir enquanto cidadão, e juntos somos a mudança que queremos para a nossa sociedade; acreditando que cada um é a mudança que deseja para o mundo, tornamos possível o sonho de uma sociedade justa e igualitária. ";
+  const valoresTexto = "Valorizar e incentivar a arte, a cultura e a educação; democratizar, por meio da cidadania cultural, o acesso aos centros culturais, esportivos, acadêmicos e profissionalizantes; desenvolver os jovens de hoje, para a sociedade de amanhã";
+
   return (
     <>
       <Header />
       <main className={`bg-brand-2 ${styles.wrapper}`}>
         <h1 className={`title-1 ${styles.textCenter}`}>Quem somos</h1>
         <section className={styles.section}>
-
+          <TextIcon textContent={missaoTexto} title="Missão" iconColor="rosa" />
+          <TextIcon textContent={visaoTexto} title="Visão" iconColor="azul" />
+          <TextIcon textContent={valoresTexto} title="Valores" iconColor="roxa" />
         </section>
       </main>
     </>
