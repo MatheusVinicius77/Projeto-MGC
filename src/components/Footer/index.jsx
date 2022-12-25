7b2f84d5d648cfe64f0af6bd582d9285adab15bc
Danielle da Luz <daniln2209@gmail.com>
2022-12-25 16:14:21 +0000

    feat: adiciona alinhamento

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,5 @@
 import "./styles.module.css";
 import styles from "./styles.module.css";
-import Fundo from "../../assets/imgs/formas/forma-preta-footer.svg";
 import instagram from "../../assets/imgs/redes-sociais/instagram-branco.svg";
 import facebook from "../../assets/imgs/redes-sociais/facebook-branco.svg";
 import linkedin from "../../assets/imgs/redes-sociais/linkedin-branco.svg";
@@ -9,17 +8,17 @@ import email from "../../assets/imgs/redes-sociais/email.svg";
 
 export default function Footer() {
   return (
-    <div>
-      <img src={logo} />
-      <ul>
+    <footer className="align-center bg-grey-1 flex">
+      <img src={logo} className={styles.logo} />
+      <ul className={`align-center flex ${styles.listIcons}`}>
         <li>
           <a href="https://www.instagram.com/projetomgc/" target="_blank">
-            `<img className="insta" src={instagram}></img>`
+            `<img src={instagram}></img>`
           </a>
         </li>
         <li>
           <a href="https://www.facebook.com/ProjetoMgc/" target="_blank">
-            `<img className="face" src={facebook}></img>`
+            `<img src={facebook}></img>`
           </a>
         </li>
         <li>
@@ -27,21 +26,23 @@ export default function Footer() {
             href="https://www.linkedin.com/company/minhasgrandescriancas/"
             target="_blank"
           >
-            `<img className="linked" src={linkedin}></img>`
+            `<img src={linkedin}></img>`
           </a>
         </li>
         <li>
-          <img className="email" src={email} />
+          <a href="">
+            <img src={email} />
+          </a>
         </li>
       </ul>
       <ul>
-        <li>Quem Somos</li>
-        <li>Depoimentos</li>
-        <li>Projetos e Oficinas</li>
-        <li>Área do Saber</li>
-        <li>Contato</li>
-        <li>Apadrinhamento</li>
+        <li className="text-1 color-grey-0">Quem Somos</li>
+        <li className="text-1 color-grey-0">Depoimentos</li>
+        <li className="text-1 color-grey-0">Projetos e Oficinas</li>
+        <li className="text-1 color-grey-0">Área do Saber</li>
+        <li className="text-1 color-grey-0">Contato</li>
+        <li className="text-1 color-grey-0">Apadrinhamento</li>
       </ul>
-    </div>
+    </footer>
   );
 }
