12be57fee75fe16a6ce39e84c4548f265be65a28
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2022-12-26 01:31:41 -0300

    feat:adciona estrutura inicial de navegação entre os depoimentos

--- src/pages/Depoimentos/index.jsx
+++ src/pages/Depoimentos/index.jsx
@@ -2,7 +2,8 @@
 import { Header } from "../../components/Header";
 import styles from "./styles.module.css";
 import { Depoimento } from "../../components/Depoimento";
-
+import setaDireita from "../../assets/imgs/icones/seta-direita.svg";
+import setaEsquerda from "../../assets/imgs/icones/seta-esquerda.svg";
 export function Depoimentopage(){
     return(
         <>
@@ -10,6 +11,7 @@ export function Depoimentopage(){
             <main className={`bg-brand-1 column flex ${styles.wrapper}`}>
             <h1 className={`title-1 ${styles.textCenter}`}>Depoimentos</h1>
                 <section className={`${styles.section} `}>
+                    {/* AINDA NÃO HÁ LÓGICA COM O BANCO DE DADOS */}
                     <div className={styles.depoimento}>
                         <Depoimento 
                         corFundoHeader="bg-brand-2" 
@@ -33,8 +35,46 @@ export function Depoimentopage(){
                         corIconeJanela="#3FB9FF" 
                         />
                     </div>
-                </section>                                   
+                </section>                                  
             </main>
+            {/* NAVEGAÇÃO POR HORA SEM FUNCIONAMENTO */}
+            <nav className={`bg-brand-1 column ${styles.nav}`}>
+                {/* SETAS POR HORA ESTÃO SEM LÓGICA */}
+                <ul className={`flex ${styles.list}`}>
+                    {/* SEM DESTAQUE DE PÁGINA ATUAL POR HORA */}
+                    <li>
+                        <button type="button" className={`bg-brand-1`}>
+                            <img src={`${setaEsquerda}`} className={styles.setaEsquerda}/>
+                        </button>
+                    </li>
+                    
+                    <li>
+                        <button type="button" className={`weight-1 title-1 bg-brand-1  ${styles.listItem} ${styles.pageStatus}`}>1</button>
+                    </li>
+
+                    <li>
+                        <button type="button" className={`weight-1 title-1 bg-brand-1  ${styles.listItem}`}>2</button>
+                    </li>
+
+                    <li>
+                        <button type="button" className={`weight-1 title-1 bg-brand-1  ${styles.listItem}`}>3</button>
+                    </li>
+
+                    <li>
+                        <h3 className={`weight-1 title-1`}>...</h3>
+                    </li>
+                    
+                    <li>
+                        <button type="button" className={`weight-1 title-1 bg-brand-1  ${styles.listItem}`}>12</button>
+                    </li>
+
+                    <li>
+                        <button type="button" className={`bg-brand-1`}>
+                            <img src={`${setaDireita}`} className={styles.setaDireita}/>
+                        </button>
+                    </li>
+                </ul>
+            </nav> 
         </>
     );
 }
