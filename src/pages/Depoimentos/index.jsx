178782404a39adccfc06ba0838c59f3bb66ab5d2
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2022-12-25 21:45:40 -0300

    feat:adciona três depoimentos manuais

--- src/pages/Depoimentos/index.jsx
+++ src/pages/Depoimentos/index.jsx
@@ -8,7 +8,32 @@ export function Depoimentopage(){
         <>
             <Header/>
             <main className={`bg-brand-1 column flex ${styles.wrapper}`}>
-            <h1 className={`title-1 ${styles.textCenter}`}>Depoimentos</h1>                                   
+            <h1 className={`title-1 ${styles.textCenter}`}>Depoimentos</h1>
+                <section className={`${styles.section} `}>
+                    <div className={styles.depoimento}>
+                        <Depoimento 
+                        corFundoHeader="bg-brand-2" 
+                        listaCoresCirculos={["bg-brand-1", "bg-brand-2", "bg-brand-3"]} 
+                        corIconeJanela="#73D676" 
+                        /> 
+                    </div>
+
+                    <div className={styles.depoimento}>
+                        <Depoimento 
+                        corFundoHeader="bg-brand-3" 
+                        listaCoresCirculos={["bg-brand-1", "bg-brand-2", "bg-brand-3"]} 
+                        corIconeJanela="#F5487E" 
+                        />
+                    </div>
+
+                    <div className={styles.depoimento}>
+                        <Depoimento 
+                        corFundoHeader="bg-brand-5" 
+                        listaCoresCirculos={["bg-brand-1", "bg-brand-2", "bg-brand-3"]} 
+                        corIconeJanela="#3FB9FF" 
+                        />
+                    </div>
+                </section>                                   
             </main>
         </>
     );
