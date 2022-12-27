fc577c3ea12183662a94980dff0af7d424b25cd4
Danielle da Luz <daniln2209@gmail.com>
2022-12-27 21:17:23 +0000

    feat: adiciona componente routes

---
+++ src/Routes/index.jsx
@@ -0,0 +1,9 @@
+import { Routes as RoutesWrapper, Route } from "react-router-dom";
+
+export function Routes() {
+  return (
+    <RoutesWrapper>
+      <Route path="/" element={} />
+    </RoutesWrapper>
+  );
+}
