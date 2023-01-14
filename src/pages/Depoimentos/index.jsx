8f8569b531ba2dacd80e7ea92c6f0b248007b298
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2023-01-14 20:18:12 -0300

    feat: adiciona cores aleatórias aos depoimentos

--- src/pages/Depoimentos/index.jsx
+++ src/pages/Depoimentos/index.jsx
@@ -8,13 +8,14 @@ import formaRoxa  from "../../assets/imgs/formas/forma-roxa-1.svg";
 
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import { RandomColor } from "./RandomColor.js/RandomColor";
 
 export function Depoimentopage(){
     const [depoimentos, setDepoimentos] = useState([])
     let [offset, setOffset] = useState(0)
 
     useEffect(()=>{
-        axios.get(`https://localhost:3001/Depoimentos/`)
+        axios.get(`https://localhost:3001/Depoimentos/` + offset)
             .then((response)=>{
                 setDepoimentos(response.data)
             })
@@ -29,15 +30,17 @@ export function Depoimentopage(){
             <main className={`bg-brand-1 column flex ${styles.wrapper}`}>
             <h1 className={`title-1 ${styles.textCenter}`}>Depoimentos</h1>
                 <section className={`${styles.section} `}>
-                    {depoimentos.map((depoimento)=>{
+                    {depoimentos.map((depoimento,key)=>{
+
+                        let color = RandomColor(key)
                         return(
                             <div className={styles.depoimento}>
                                 <Depoimento
-                                corFundoHeader="bg-brand-5" 
-                                listaCoresCirculos={["bg-brand-1", "bg-brand-2", "bg-brand-3"]} 
-                                corIconeJanela="#3FB9FF" 
-                                depoimentoObject={depoimento}
-                                />
+                                corFundoHeader={color[0]}
+                                listaCoresCirculos={color[1]} 
+                                corIconeJanela={color[2]}
+                                depoimentoObject = {depoimento}
+                                /> 
                             </div>
                         )
                     })} 
