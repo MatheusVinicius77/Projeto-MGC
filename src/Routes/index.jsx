fd36af047a723d52a655c13b6b4dc19f3966cbce
Danielle-Luz <daniln2209@gmail.com>
2023-01-24 09:49:24 -0300

    feat: adiciona rota área do saber e projetos

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,5 +1,7 @@
 import { Routes as RoutesWrapper, Route, Navigate } from "react-router-dom";
+import { AreaDoSaber } from "../pages/AreaDoSaber";
 import { Homepage } from "../pages/Homepage";
+import { Projetos } from "../pages/Projetos";
 import { QuemSomos } from "../pages/QuemSomos";
 
 export function Routes() {
@@ -8,6 +10,8 @@ export function Routes() {
       <RoutesWrapper>
         <Route path="/home" element={<Homepage />} />
         <Route path="/quemsomos" element={<QuemSomos />} />
+        <Route path="/projetos" element={<Projetos />} />
+        <Route path="/areadosaber" element={<AreaDoSaber />} />
         <Route path="*" element={<Navigate to="/home" />} />
       </RoutesWrapper>
     </>
