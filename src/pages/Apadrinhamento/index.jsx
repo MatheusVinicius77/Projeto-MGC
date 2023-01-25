10d7e2b19101e55903ee0cdde5dbd28b0ba1035e
Danielle-Luz <daniln2209@gmail.com>
2023-01-24 21:30:34 -0300

    feat: adiciona card na página de apadrinhamento

--- src/pages/Apadrinhamento/index.jsx
+++ src/pages/Apadrinhamento/index.jsx
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 import { Header } from "../../components/Header";
+import { Card } from "../../components/Cards";
+import { Button } from "../../components/Button";
+import CIEE from "../../assets/imgs/fotos/cie.svg";
 import Footer from "../../components/Footer";
 import styles from "./styles.module.css";
 
@@ -10,12 +13,39 @@ export function Apadrinhamento() {
   return (
     <>
       <Header />
-      <main className={`bg-brand-2 column flex ${styles.main}`}>
-        <div className={`align-center column flex ${styles.titleWrapper}`}>
-          <h1 className="title-1">Apadrinhe uma Criança</h1>
-          <h3 className="text-2">
-            "Juntos podemos ser a mudança que queremos para o mundo"
-          </h3>
+      <main className={`bg-brand-2 flex justify-center ${styles.main}`}>
+        <div className="container column flex">
+          <section
+            className={`align-center column flex ${styles.titleWrapper}`}
+          >
+            <h2 className="title-1">Apadrinhe uma Criança</h2>
+            <p className="text-2">
+              "Juntos podemos ser a mudança que queremos para o mundo"
+            </p>
+          </section>
+          <article className={`align-center flex justify-between ${styles.sectionConteudo}`}>
+            <Card
+              corFundoHeader="bg-brand-4"
+              listaCoresCirculos={["bg-brand-1", "bg-brand-2", "bg-brand-5"]}
+              imagem={CIEE}
+              textoAlternativoImagem="Centro de Integração Empresa-Escola"
+              coresSinal={["#BC54F0", "#F5487E"]}
+              corIconeJanela="#73D676"
+            />
+            <section className={`column flex ${styles.texto}`}>
+              <div className="column flex">
+                <p className="text-2">
+                  Apadrinhar uma criança é um ato de amor! Acolher um futuro é
+                  abrir uma infinidade de possibilidades a quem precisa!
+                  Apadrinhe uma criança e seja parte da transformação!
+                </p>
+                <p className="text-2">
+                  Para apadrinhar uma criança entre em contato conosco!
+                </p>
+              </div>
+              <Button title="Whatsapp" />
+            </section>
+          </article>
         </div>
       </main>
       <Footer />
